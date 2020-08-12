2d0d9aaaff207ef033d6d6ab54933345a1e7e2d4
pandanon50 <60589856+pandanon50@users.noreply.github.com>
2020-08-12 22:08:34 +0900

    front header,footer,home with scss

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-import express from 'express';
-import 'core-js';
-import routes from './routes.js';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import bodyparser from 'body-parser';
-import cookieparser from 'cookie-parser';
-import { localsMiddleware } from './middlewares';
+import express from "express";
+import "core-js";
+import routes from "./routes.js";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyparser from "body-parser";
+import cookieparser from "cookie-parser";
+import { localsMiddleware } from "./middlewares";
 
-import globalRouter from './routers/globalRouter';
-import userRouter from './routers/userRouter';
-import meetRouter from './routers/meetRouter';
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import meetRouter from "./routers/meetRouter";
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
 app.use(localsMiddleware);
 app.use(helmet());
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieparser());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require("path");
-const autoprefixer = require("autoprefixer");
-const ExtractCSS = require("extract-text-webpack-plugin");
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
@@ -21,25 +19,7 @@ const config = {
       },
       {
         test: /\.(scss)$/,
-        use: ExtractCSS.extract([
-          {
-            // css를 load
-            loader: "css-loader",
-          },
-          {
-            // 특정 plugin들을 css에 대해 실행
-            loader: "postcss-loader",
-            options: {
-              plugins() {
-                return [autoprefixer({ browsers: "cover 99.5%" })];
-              },
-            },
-          },
-          {
-            // Sass를 css로 변환
-            loader: "sass-loader",
-          },
-        ]),
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
@@ -47,7 +27,11 @@ const config = {
     path: OUTPUT_DIR,
     filename: "[name].js",
   },
-  plugins: [new ExtractCSS("styles.css")],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
 };
 
 module.exports = config;
